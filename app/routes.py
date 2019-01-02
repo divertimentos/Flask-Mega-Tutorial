30f83b81ef3918986edb97602d0b07e148bee971
guiemi <guilherme@vert-capital.com>
2019-01-02 12:21:55 -0200

    Finaliza o capítulo sobre templates

--- app/routes.py
+++ app/routes.py
@@ -4,17 +4,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'World'}
+    user = {'username': 'User'}
     posts = [
         {
-        'author': {'username': 'John'},
-        'body': 'Beautiful day in Portland!'
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
         {
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
+
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 # render_template()" é uma função built-in do Flask que invoca o Jinja2
